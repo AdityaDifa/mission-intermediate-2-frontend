270b8948fdddd3d816b8b3278808235026eb7ce9
Aditya Difa Amanullah <adityadifa123@gmail.com>
2025-10-24 10:50:39 +0700

    flag icon done

--- src/assets/components/inputs/InputTelpFlagComponent.tsx
+++ src/assets/components/inputs/InputTelpFlagComponent.tsx
@@ -1,3 +1,6 @@
+import CountryList from "country-list-with-dial-code-and-flag";
+import { useEffect, useState } from "react";
+
 type TInputTelpFlagComponent = {
   id: string;
   value: string;
@@ -9,10 +12,34 @@ export default function InputTelpFlagComponent({
   value,
   setValue,
 }: TInputTelpFlagComponent) {
+  const [flag, setFlag] = useState("https://flagcdn.com/16x12/ca.png");
+  const [keyFlag, setKeyFlag] = useState("");
+
+  function getAllDialCode() {
+    const allDialCode: Array<any> = CountryList.getAll();
+    const sortedData = allDialCode.sort(
+      (a, b) => parseInt(a.dial_code) - parseInt(b.dial_code)
+    );
+    // sortedData.forEach((data) => console.log(data.data)); //<--just for testing
+    return sortedData.map((data) => (
+      <option
+        key={`${data.data.name} ${data.data.dial_code}`}
+        data-code={data.data.code}
+      >
+        {data.data.dial_code}
+      </option>
+    ));
+  }
+
+  useEffect(() => {
+    setFlag(`https://flagcdn.com/16x12/${keyFlag}.png`);
+    // console.log(flag); //just for testing
+  }, [keyFlag]);
+
   return (
     <div className="flex border-[#3A35411F] rounded-[6px] w-full">
-      <div className="basis-1/4 bg-[#F4F5FA] border-r border-r-[#3A35411F] px-[10px] py-[4px]">
-        <div></div>
+      <div className="basis-1/4 bg-[#F4F5FA] border-r border-r-[#3A35411F] px-[10px] py-[4px] p-1">
+        <img src={flag} alt="" className="w-[24px]" />
       </div>
       <div className="basis-3/4 w-full">
         <select
@@ -20,7 +47,14 @@ export default function InputTelpFlagComponent({
           name="phoneId"
           id={id}
           value={value}
-        ></select>
+          onChange={(e) => {
+            setValue(e.target.value);
+            const selectedOption: any = e.target.selectedOptions[0];
+            setKeyFlag(selectedOption.getAttribute("data-code").toLowerCase());
+          }}
+        >
+          {getAllDialCode()}
+        </select>
       </div>
     </div>
   );
